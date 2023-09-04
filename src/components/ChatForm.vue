1caf7dceb87f8920e812ad4759037d5cbfbdf652
VishalCoder78 <vishalcoder78@gmail.com>
2023-09-04 13:17:15 +0530

    pheoenix 2.0 frontend complete

--- src/components/ChatForm.vue
+++ src/components/ChatForm.vue
@@ -19,14 +19,14 @@ export default{
 <template>
     <div class="h-[6vh] mt-2 mx-1 rounded-md bg-gray-300 flex items-center">
         <div class="flex">
-            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                <ShareIcon class="h-4 w-6 text-white" />
+            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md">
+                <ShareIcon class="h-8 w-8 text-white" />
             </button>
-            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                <CameraIcon class="h-4 w-6 text-white" />
+            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-0.5">
+                <CameraIcon class="h-4 w-8 text-white" />
             </button>
-            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                <FaceSmileIcon class="h-4 w-6 text-white" />
+            <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md">
+                <FaceSmileIcon class="h-4 w-8 text-white" />
             </button>
         </div>
         
@@ -35,7 +35,7 @@ export default{
         </div>
         <div>
             <button @click="toggleUserList" class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1 flex">
-                <UserIcon class="h-4 w-4 text-white" /><svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-arrows-angle-expand " viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z" fill="white"></path> </svg>
+                <UserIcon class="h-8 w-8 text-white" /><svg style="color: white" xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-arrows-angle-expand " viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707zm4.344-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707z" fill="white"></path> </svg>
             </button>
         </div>
     </div>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -29,29 +29,29 @@ export default {
     <div class="h-[7vh] flex justify-between items-center">
         <div class="flex">
             <div class="mx-4">
-                <img class="mt-2" src="../assets/logo.svg" alt="">
+                <img class="my-4" src="../assets/logo.svg" alt="">
             </div>
             <div class="">
                 <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                    <Cog8ToothIcon class="h-6 w-6 text-white" />
+                    <Cog8ToothIcon class="h-8 w-8 text-white" />
                 </button>
                 <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                    <Bars3Icon class="h-6 w-6 text-white" />
+                    <Bars3Icon class="h-8 w-8 text-white" />
                 </button>
                 <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                    <EnvelopeIcon class="h-6 w-6 text-white" />
+                    <EnvelopeIcon class="h-8 w-8 text-white" />
                 </button>
                 <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                    <QuestionMarkCircleIcon class="h-6 w-6 text-white" />
+                    <QuestionMarkCircleIcon class="h-8 w-8 text-white" />
                 </button>
                 <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                    <PlayIcon class="h-6 w-6 text-white" />
+                    <PlayIcon class="h-8 w-8 text-white" />
                 </button>
             </div>
         </div>
         <div v-if="isUserListVisible" class="nav-cross">
             <button class="bg-gradient-to-b from-yellow-700 to-yellow-500 p-2 mt-1 rounded-md mx-1">
-                <XCircleIcon @click="toggleUserList" class="h-6 w-6 text-white" />
+                <XCircleIcon @click="toggleUserList" class="h-8 w-8 text-white" />
             </button>
         </div>
     </div>
